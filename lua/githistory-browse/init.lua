038aa1c0e8d90cc255ef3106e06f69f046d1d527
engeir <eirroleng@gmail.com>
2022-02-20 13:56:30 +0100

    :tada: First dirty and working implementation
    * Calling `GhBrowse` from git repo file opens up githistory.xyz!

--- lua/githistory-browse/init.lua
+++ lua/githistory-browse/init.lua
@@ -1,5 +1,9 @@
 local M = {}
 
+local function isempty(s)
+    return s == nil or s == ""
+end
+
 local function mysplit(inputstr, sep)
     -- https://stackoverflow.com/a/7615129/10642998
     if sep == nil then
@@ -19,21 +23,27 @@ local function replace(str, what, with)
     return string.gsub(str, what, with)
 end
 
-M.url = function()
-    -- local current_file = vim.api.nvim_get_runtime_file(filename, true)
+M.browse_file = function()
     local current_file = vim.fn.expand("%")
     local current_dir = vim.api.nvim_exec("pwd", true)
     local git_remote0 = vim.api.nvim_exec("!git config --get remote.origin.url", true)
     local git_remote = mysplit(git_remote0, "\n")[2]
+    local git_remote = string.gsub(git_remote, "%.git", "")
     local git_local0 = vim.lsp.buf.list_workspace_folders()
     local git_local = git_local0[1]
     local git_hist, _ = string.gsub(git_remote, "com", "githistory.xyz")
-    local relative_path, _ = replace(current_dir, git_local, "")
+    if not isempty(current_dir) and not isempty(git_local) then
+        local relative_path, _ = replace(current_dir, git_local, "")
 
-    local out = git_hist .. "/blob/main" .. relative_path .. "/" .. current_file
-    print(out)
+        local out = git_hist .. "/blob/main" .. relative_path .. "/" .. current_file
+        vim.api.nvim_command("call netrw#BrowseX('" .. out .. "', netrw#CheckIfRemote())")
+    else
+        local out = ""
+        print("No git repo found")
+    end
+    return current_file
 end
 
-M.url()
+vim.api.nvim_add_user_command("GhBrowse", M.browse_file, {})
 
 return M
