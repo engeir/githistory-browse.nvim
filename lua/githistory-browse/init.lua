73a77bcfb159c819df741c834f8ba97af6b09979
engeir <eirroleng@gmail.com>
2022-02-20 15:53:07 +0100

    Run `git branch` to check which is active, fall back to main

--- lua/githistory-browse/init.lua
+++ lua/githistory-browse/init.lua
@@ -23,28 +23,50 @@ local function replace(str, what, with)
     return string.gsub(str, what, with)
 end
 
+local function get_git_remote()
+    local git_remote0 = vim.api.nvim_exec("!git config --get remote.origin.url", true)
+    local git_remote = mysplit(git_remote0, "\n")[2]
+    return string.gsub(git_remote, "%.git", "")
+end
+
+local function get_git_local()
+    local git_local = vim.lsp.buf.list_workspace_folders()
+    return git_local[1]
+end
+
+local function git_default_branch()
+    local git_b_list0 = vim.api.nvim_exec("!git branch", true)
+    local git_b_list = mysplit(git_b_list0, "\n")
+    table.remove(git_b_list, 1)
+    for i, v in pairs(git_b_list) do
+        if v:find("*") then
+            return mysplit(v, " ")[2]
+        end
+    end
+    return "main"
+end
+
 M.browse_file = function()
     local current_file = vim.fn.expand("%")
     local current_dir = vim.api.nvim_exec("pwd", true)
-    local git_remote0 = vim.api.nvim_exec("!git config --get remote.origin.url", true)
-    local git_remote = mysplit(git_remote0, "\n")[2]
-    local git_remote = string.gsub(git_remote, "%.git", "")
-    local git_local0 = vim.lsp.buf.list_workspace_folders()
-    local git_local = git_local0[1]
+    local git_remote = get_git_remote()
+    local git_local = get_git_local()
     local git_hist, _ = string.gsub(git_remote, "com", "githistory.xyz")
+    local git_default_branch = git_default_branch()
     if not isempty(current_dir) and not isempty(git_local) then
         local relative_path, _ = replace(current_dir, git_local, "")
 
-        local out = git_hist .. "/blob/main" .. relative_path .. "/" .. current_file
+        local out = git_hist .. "/blob/" .. git_default_branch .. relative_path .. "/" .. current_file
         vim.api.nvim_command("call netrw#BrowseX('" .. out .. "', netrw#CheckIfRemote())")
-    elseif not isempty(git_remote) then
-        local git_repo = mysplit(git_remote, "/")
+    elseif not isempty(git_hist) then
+        local git_repo = mysplit(git_hist, "/")
         local repo_name = git_repo[#git_repo]
         local relative_to_repo = mysplit(current_dir, "/")
         for i, v in pairs(relative_to_repo) do
             if v == repo_name then
                 local out = git_hist
-                    .. "/blob/main"
+                    .. "/blob/"
+                    .. git_default_branch
                     .. "/"
                     .. table.concat(relative_to_repo, "/", i + 1)
                     .. "/"
